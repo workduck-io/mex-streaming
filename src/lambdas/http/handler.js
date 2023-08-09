71aeba63c6de0a30250e81d1f00f034a40a0ad93
Varun Garg <varungarg@Varuns-MacBook-Pro-2.local>
2023-08-09 19:24:15 +0530

    flowwise

--- src/lambdas/http/handler.js
+++ src/lambdas/http/handler.js
@@ -1,6 +1,7 @@
 import { isAuthorized } from "../../utils/authorizeUtils";
 import { extractToken } from "../../utils/tokenUtils";
 import { getMessagesInLangchainFormat } from "../../utils/messageUtils";
+import { invokeAgent } from "../../utils/flowiseUtils";
 import { getDetails, updateTokenUsage } from "../../utils/lambdaUtils";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 
@@ -26,10 +27,13 @@ async function initiateChat(apiKey, messages, responseStream) {
 
 exports.handleRequest = awslambda.streamifyResponse(async function (event, responseStream, context) {
     try {
+        console.log(context)
         const httpResponseMetadata = {
             status: 200,
             headers: {
                 'Content-Type': 'text/html',
+                "Access-Control-Allow-Origin": "*", // Required for CORS support to work
+                "Access-Control-Allow-Credentials": true, // Required for cookies, authorization headers with HTTPS
             },
         }
 
@@ -38,13 +42,25 @@ exports.handleRequest = awslambda.streamifyResponse(async function (event, respo
         const bearerToken = extractToken(event.headers.authorization);
         await isAuthorized(bearerToken, workspaceId);
 
-        const { apiKey, isSystemToken, messages } = await getDetails(event.body, workspaceId, bearerToken);
+        const requestType = JSON.parse(event.body).type
+        console.log("Event.Body.Type parsed = " + requestType)
+        if (requestType === 'agent') {
+            // TODO : check for existence of question field in the body.
+            // TODO : add support for multiple intents.
+            invokeAgent(event.body, responseStream)
 
-        await initiateChat(apiKey, messages, responseStream);
+        } else {
+            /* call lambda to get apiKey to use & messages in desired format */
+            const { apiKey, isSystemToken, messages } = await getDetails(event.body, workspaceId, bearerToken);
 
-        if (isSystemToken) {
-            await updateTokenUsage(workspaceId, bearerToken);
+            await initiateChat(apiKey, messages, responseStream);
+
+            if (isSystemToken) {
+                await updateTokenUsage(workspaceId, bearerToken);
+            }
         }
+
+
     } catch (error) {
         console.log(error);
 
